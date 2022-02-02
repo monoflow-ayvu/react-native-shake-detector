4fc23aead4bee095fbfb60557f220b751033d0dc
Fernando Mumbach <fermuch@ayvu.net>
2022-02-02 09:45:59 -0300

    adding stop method

--- android/src/main/java/com/fernandomumbach/reactnativeshakedetector/RNShakeDetectorModule.kt
+++ android/src/main/java/com/fernandomumbach/reactnativeshakedetector/RNShakeDetectorModule.kt
@@ -7,7 +7,7 @@ import android.content.IntentFilter
 import com.facebook.react.bridge.*
 import com.facebook.react.modules.core.DeviceEventManagerModule
 
-class RNShakeDetectorModule(reactContext: ReactApplicationContext) : ReactContextBaseJavaModule(reactContext) {
+class RNShakeDetectorModule(reactContext: ReactApplicationContext) : ReactContextBaseJavaModule(reactContext), LifecycleEventListener {
     private val LOG_TAG = "RNShakeDetectorModule"
     private var mReactContext: ReactApplicationContext = reactContext
     private var mApplicationContext: Context = reactContext.applicationContext
@@ -24,7 +24,7 @@ class RNShakeDetectorModule(reactContext: ReactApplicationContext) : ReactContex
             val action = intent.action
             if (action != null && action == ShakeServiceConstants.ACTION_SHAKE) {
                 val percentOverThreshold =
-                    intent.getDoubleExtra(ShakeServiceConstants.EXTRA_PERCENT_OVER_THRESHOLD, 0.0)
+                    intent.getDoubleExtra(ShakeServiceConstants.EXTRA_PERCENT_OVER_THRESHOLD, -1.0)
                 val ev = Arguments.createMap()
                 ev.putDouble("percentOverThreshold", percentOverThreshold)
 
@@ -39,36 +39,64 @@ class RNShakeDetectorModule(reactContext: ReactApplicationContext) : ReactContex
 
     @ReactMethod
     fun start(
-        MAX_SAMPLES: Int,
-        MIN_TIME_BETWEEN_SAMPLES_MS: Int,
-        VISIBLE_TIME_RANGE_MS: Int,
-        MAGNITUDE_THRESHOLD: Int,
-        PERCENT_OVER_THRESHOLD_FOR_SHAKE: Int,
+        maxSamples: Int,
+        MinTimeBetweenSamplesMs: Int,
+        VisibleTimeRangeMs: Int,
+        MagnitudeThreshold: Int,
+        PercentOverThresholdForShake: Int,
         promise: Promise
     ) {
         try {
             val intent = Intent(mApplicationContext, ShakeService::class.java)
-            intent.putExtra(ShakeServiceConstants.EXTRA_CONFIG_MAX_SAMPLES, MAX_SAMPLES)
+            intent.putExtra(ShakeServiceConstants.EXTRA_CONFIG_MAX_SAMPLES, maxSamples)
             intent.putExtra(
                 ShakeServiceConstants.EXTRA_CONFIG_MIN_TIME_BETWEEN_SAMPLES_MS,
-                MIN_TIME_BETWEEN_SAMPLES_MS
+                MinTimeBetweenSamplesMs
             )
             intent.putExtra(
                 ShakeServiceConstants.EXTRA_CONFIG_VISIBLE_TIME_RANGE_MS,
-                VISIBLE_TIME_RANGE_MS
+                VisibleTimeRangeMs
             )
             intent.putExtra(
                 ShakeServiceConstants.EXTRA_CONFIG_MAGNITUDE_THRESHOLD,
-                MAGNITUDE_THRESHOLD
+                MagnitudeThreshold
             )
             intent.putExtra(
                 ShakeServiceConstants.EXTRA_PERCENT_OVER_THRESHOLD,
-                PERCENT_OVER_THRESHOLD_FOR_SHAKE
+                PercentOverThresholdForShake
             )
             mApplicationContext.startService(intent)
             return promise.resolve(true)
         } catch (e: Exception) {
             promise.reject("Could not start ShakeService", e)
         }
+    }
+
+    @ReactMethod
+    fun stop(promise: Promise) {
+        try {
+            internalStop()
+            promise.resolve(true)
+        } catch (e: Exception) {
+            promise.reject("Could not stop ShakeService", e)
+        }
+    }
+
+    private fun internalStop() {
+        val intent = Intent(mApplicationContext, ShakeService::class.java)
+        mApplicationContext.stopService(intent)
+    }
+
+    override fun onHostResume() {
+        return
+    }
+
+    override fun onHostPause() {
+        return
+    }
+
+    override fun onHostDestroy() {
+        mReactContext.unregisterReceiver(mReceiver)
+        internalStop()
     }
 }
